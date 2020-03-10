90dd72429335f9556cd8cf07cb355a874ee6cfa5
Pedro Siqueira <pedro.v.siqueira@gmail.com>
2020-03-10 19:33:45 -0300

    Fixing image problem when system is deployed

--- canvas.js
+++ canvas.js
@@ -242,9 +242,9 @@ var backgroundImage = {
     draw: function () {
         ctx.drawImage( this.img, this.x, 0 );
         if ( this.speed < 0 ) {
-            ctx.drawImage( this.img, this.x + canvas.width, 0 );
+            ctx.drawImage( img, this.x + canvas.width, 0 );
         } else {
-            ctx.drawImage( this.img, this.x - this.img.width, 0 );
+            ctx.drawImage( img, this.x - this.img.width, 0 );
         }
     },
 };
